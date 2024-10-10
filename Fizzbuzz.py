a7f1b06c01437cae621f62f83f95675463717d2c
naepita <9rigakiraina193@gmail.com>
2024-10-10 21:27:48 +0900

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,13 @@
+num = 1
+max_num = int(input("数字を入力してください"))
+
+while(num <= max_num):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    num += 1
